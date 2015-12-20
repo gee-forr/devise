3f01d8fb878fb73295482124f7754d69f688de46
Gabriel Fortuna <gee.forr@gmail.com>
2015-12-20 23:11:41 +0200

    hack: force devise to use user mappings

--- lib/devise/omniauth.rb
+++ lib/devise/omniauth.rb
@@ -14,7 +14,7 @@ end
 OmniAuth.config.path_prefix = nil
 
 OmniAuth.config.on_failure = Proc.new do |env|
-  env['devise.mapping'] = Devise::Mapping.find_by_path!(env['PATH_INFO'], :path)
+  env['devise.mapping'] = Devise.mappings[:user]
   controller_name  = ActiveSupport::Inflector.camelize(env['devise.mapping'].controllers[:omniauth_callbacks])
   controller_klass = ActiveSupport::Inflector.constantize("#{controller_name}Controller")
   controller_klass.action(:failure).call(env)
